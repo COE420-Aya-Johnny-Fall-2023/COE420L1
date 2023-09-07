614463f018e02798cbf62a6e9c10729743f498eb
AyaZabalawi <g00089004@aus.edu>
2023-09-07 21:04:54 +0400

    Feature B added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 
 public class Main {
 
-	public static void main(String[] args) {
+	public void FeatureB()
+	{
+		System.out.println("Feature B\n");
+	}
+
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		System.out.print("Hello World\n");
 	}
